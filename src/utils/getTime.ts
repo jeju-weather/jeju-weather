ff1866bfc8b843c12d780030b26e99fafc71952c
박지윤 <jyun1937@naver.com>
2023-01-28 20:33:44 +0900

    Feat: getTime util 함수 수정 - 10 이하 숫자는 0붙도록

--- src/utils/getTime.ts
+++ src/utils/getTime.ts
@@ -1,9 +1,10 @@
 export const getTime = (time: number) => {
   const convertTime = new Date(time * 1000).getTime();
   const week = ['일', '월', '화', '수', '목', '금', '토'];
+  let setZero = (time: number) => (time > 10 ? time : `0${time}`);
   return {
-    hour: new Date(convertTime).getHours(),
-    minutes: new Date(convertTime).getMinutes(),
+    hour: setZero(new Date(convertTime).getHours()),
+    minutes: setZero(new Date(convertTime).getMinutes()),
     month: new Date(convertTime).getMonth() + 1,
     date: new Date(convertTime).getDate(),
     day: week[new Date(convertTime).getDay()],
