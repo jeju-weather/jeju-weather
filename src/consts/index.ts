f870c48000e4e97052b2638b616b0d9d4c95a93c
박지윤 <jyun1937@naver.com>
2023-03-21 16:28:39 +0900

    Refactor: 한번만 사용하는 변수 수정 및 이동

--- src/consts/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './WeatherInfo';
-export * from './location';
--- src/store/modules/weatherInfo/index.ts
+++ src/store/modules/weatherInfo/index.ts
@@ -1,7 +1,51 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { getWeather } from 'apis';
-import { initialWeatherState } from 'consts';
-import { GetWeatherPrams } from 'types';
+import { GetWeatherPrams, WeatherInfoTypes } from 'types';
+
+const initialWeatherState: WeatherInfoTypes = {
+  current: {
+    humidity: 0,
+    sunrise: 0,
+    sunset: 0,
+    temp: 0,
+    wind_speed: 0,
+    rain: { '1h': 0 },
+    weather: [
+      {
+        icon: '',
+      },
+    ],
+  },
+  daily: [
+    {
+      dt: 0,
+      temp: {
+        min: 0,
+        max: 0,
+      },
+      weather: [
+        {
+          icon: '',
+        },
+      ],
+    },
+  ],
+  hourly: [
+    {
+      dt: 0,
+      humidity: 0,
+      temp: 0,
+      rain: { '1h': 0 },
+      snow: { '1h': 0 },
+      wind_speed: 0,
+      weather: [
+        {
+          icon: '',
+        },
+      ],
+    },
+  ],
+};
 
 export const getWeatherInfo = createAsyncThunk(
   'Jeju/getWeatherInfo',
