0794072f4a6545378404cf778a8cca7027376c4c
sso-hyeon <ashash1994@naver.com>
2023-04-20 16:55:59 +0900

    Fix: useAxios params 사용하여 serviceKey 넘겨주기

--- src/pages/Hallasan/index.tsx
+++ src/pages/Hallasan/index.tsx
@@ -11,7 +11,10 @@ export const Hallasan = () => {
   const { isLoading, data } = useAxios<HallasanInfoTypes>({
     queryId: 'hallasanInfo',
     method: 'get',
-    url: `https://api.odcloud.kr/api/15056441/v1/uddi:1635e441-a1c9-42c1-95a8-a2b4891245bb_201911141055?serviceKey=${process.env.REACT_APP_HANLA_MOUNTAIN_KEY}`,
+    url: `https://api.odcloud.kr/api/15056441/v1/uddi:1635e441-a1c9-42c1-95a8-a2b4891245bb_201911141055`,
+    params: {
+      serviceKey: decodeURIComponent(process.env.REACT_APP_HANLA_MOUNTAIN_KEY),
+    },
   });
 
   if (isLoading) {
